957400622cff6de26f3dd89e47ac8000f5c792bf
Spencer <241108@st.yoshida-g.ac.jp>
2026-06-11 08:40:21 +0900

    ci: GitHub Actions・Prettier・ESLintを追加

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -17,5 +18,9 @@ export default defineConfig([
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
+    rules: {
+      'no-console': 'warn',
+    },
   },
+  prettierConfig,
 ])
